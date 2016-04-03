ebea5fb10bd3ddccfdc6df3c0469126763a9ec6a
小菅仁士 <satoshi082015@gmail.com>
2016-04-03 16:04:43 +0900

    import Photos to CreateNewTeamViewController Class add authorication of PhotoLibrary in case of NotDetermined and Authorized to pickFeaturedImage_Clicked function of CreateNewTeamViewController add UIImagePickerController to presentCamera function of CreateNewTeamViewController Class

--- imessagesclone/CreateNewTeamViewController.swift
+++ imessagesclone/CreateNewTeamViewController.swift
@@ -7,29 +7,67 @@
 //
 
 import UIKit
+import Photos
 
-class CreateNewTeamViewController: UIViewController {
+class CreateNewTeamViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
+    @IBOutlet weak var teamFeaturedImage: UIImageView!
+    @IBOutlet weak var newTeamTextField: UITextField!
+    @IBOutlet weak var containerView: DesignableView!
 
+    private var featuredImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        newTeamTextField.becomeFirstResponder()
+        teamFeaturedImage.layer.cornerRadius = teamFeaturedImage.layer.bounds.width / 2
+        teamFeaturedImage.clipsToBounds = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func pickFeaturedImage_Clicked(sender: UITapGestureRecognizer) {
+        
+        let authorication = PHPhotoLibrary.authorizationStatus()
+        
+        if authorication == .NotDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.pickFeaturedImage_Clicked(sender)
+                })
+            })
+            return
+        }
+        
+        if authorication == .Authorized {
+            let controller = ImagePickerSheetController()
+            controller.addAction(ImageAction(title: NSLocalizedString("take Photo or video", comment: "ActionTitle"), secondaryTitle: NSLocalizedString("Use this one", comment: "ActionTitle"), handler: { (_) -> () in
+                    self.presentCamera()
+                
+                
+                }, secondaryHandler: { (action, numberOfPhotos) -> () in
+                    controller.getSelectedImagesWithCompletion({ (images) -> Void in
+                        self.featuredImage = images[0]
+                        self.teamFeaturedImage.image = self.featuredImage
+                    })
+            }))
+            
+            controller.addAction(ImageAction(title: NSLocalizedString("Cancel", comment: "ActionTitle"), style: .Cancel, handler: nil, secondaryHandler: nil))
+            presentViewController(controller, animated: true, completion: nil)
+        }
     }
-    */
-
+    
+    private func presentCamera() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func backButton_Clicked(sender: UIButton) {
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    @IBAction func createButton_Clicked(sender: UIButton) {
+        
+    }
 }
